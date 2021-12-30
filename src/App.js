ff52b96f5b390f72d4751bcb00574249b19e6c71
bocheng24 <jeffcgp@hotmail.com>
2021-12-30 21:08:45 +0800

    layout the main app structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import styled from 'styled-components';
 import './App.css';
 
+import Sidebar from './components/Sidebar';
+import Chatlist from './components/Chatlist';
+import Chatview from './components/Chatview';
+
 const Wrapper = styled.div`
+  background-color: #282a37;
+  min-height: 100vh;
+  max-height: 100vh;
+  min-width: 100vw;
+  max-width: 100vw;
+`;
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 1;
 `;
 
 function App() {
   return (
     <Wrapper>
-      
+      <Sidebar />
+      <Chatlist />
+      <Main>
+        <Chatview />
+      </Main>
     </Wrapper>
   );
 }
---
+++ src/components/Chatview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chatview() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chatview
