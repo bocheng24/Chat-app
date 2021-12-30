efadfd23a39b8bd7617ed2d8cdb9c10832f957c7
bocheng24 <jeffcgp@hotmail.com>
2021-12-30 20:58:36 +0800

    Init the project

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyB-JB1Pf9ESLr36qBa_gmxctsK_x9JNq4Q",
+    authDomain: "chat-app-971a4.firebaseapp.com",
+    projectId: "chat-app-971a4",
+    storageBucket: "chat-app-971a4.appspot.com",
+    messagingSenderId: "370904825826",
+    appId: "1:370904825826:web:be08c1ef2476c1d45743bb"
+  });
+
+const db = getFirestore();
+const provider = GoogleAuthProvider();
+
+export { db, provider }
