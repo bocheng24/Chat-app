15c100e611c3a7aa953365cb46d9982d0886cf0b
bocheng24 <jeffcgp@hotmail.com>
2021-12-30 22:13:23 +0800

    style the side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,20 +10,59 @@ const Wrapper = styled.div`
     width: 100px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
 `;
 
 const LogoContainer = styled.div`
+    object-fit: contain;
+    height: 70px;
+
+    img {
+        height: 100%;
+    }
 
 `;
 
 const SideIcons = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
 
+const SideIcon = styled.div`
+    i {
+        color: #95a5a6;
+        font-size: 28px;
+        padding: 28px;
+        cursor: pointer;
+
+        &:hover {
+            color: #ff6b6b;
+        }
+    }
 `;
 
 const ProfileIcon = styled.div`
+    object-fit: contain;
+    height: 70px;
+    cursor: pointer;
 
+    img {
+        height: 100%;
+        border-radius: 50%;
+
+        &:hover {
+            --webkit-box-shadow: 0 5px 24px -3px #ff6b6b;
+            box-shadow: 0 5px 24px -3px #ff6b6b;
+        }
+    }
+
+    &:hover {
+        transform: scale(1.05);
+    }
 `;
 
 function Sidebar() {
@@ -31,23 +70,25 @@ function Sidebar() {
         <Wrapper>
 
             <LogoContainer>
-                <img style={ {width: '100px', borderRadius: '50%'} } src={ logo } alt="" />
+                <img src={ logo } alt="" />
             </LogoContainer>
 
             <SideIcons>
-                <i className="fas fa-inbox"></i>
-            </SideIcons>
-            <SideIcons>
-                <i className="fas fa-cog"></i>
-            </SideIcons>
-            <SideIcons>
-                <i className="fas fa-user-circle"></i>
-            </SideIcons>
-            <SideIcons>
-                <i className="fas fa-bolt"></i>
+                <SideIcon>
+                    <i className="fas fa-inbox"></i>
+                </SideIcon>
+                <SideIcon>
+                    <i className="fas fa-cog"></i>
+                </SideIcon>
+                <SideIcon>
+                    <i className="fas fa-user-circle"></i>
+                </SideIcon>
+                <SideIcon>
+                    <i className="fas fa-bolt"></i>
+                </SideIcon>
             </SideIcons>
             <ProfileIcon>
-                <img style={ {width: '100px', borderRadius: '50%'} } src={ profileImg } alt="" />
+                <img src={ profileImg } alt="" />
             </ProfileIcon>
         </Wrapper>
     )
